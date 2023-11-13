cd87362bc47dadb777825bdfde3bc5ca8000d598
diegosg99 <diegosg22360@gmail.com>
2023-11-13 12:21:09 +0100

    Preguntas calentitas en tos laos

--- src/app/views/forum/forum-question/forum-question.component.ts
+++ src/app/views/forum/forum-question/forum-question.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, debounceTime } from 'rxjs';
 import { ForumService } from 'src/app/services/forum.service';
@@ -14,6 +14,7 @@ import { UserService } from 'src/app/services/user.service';
 export class ForumQuestionComponent implements OnInit{
 
   idQuestion:any = this._activatedroute.snapshot.paramMap.get('id');
+  $questions:Observable<any> = this.forumService.getQuestions();
   //@Output() commentsDiv:any = new EventEmitter<string>();
   question:any;
   userId:any;
